a36d0f583fd902bb61cbdfcc31c35f29b316c4e6
dovietchinh <dovietchinh1998@gmai.com>
2022-04-10 19:32:34 +0700

    problem6

--- source/problem5.cpp
+++ source/problem5.cpp
@@ -1,11 +1,68 @@
 /*
+link : https://leetcode.com/problems/roman-to-integer/
 
-link : https://leetcode.com/problems/palindrome-number/
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
-Given an integer x, return true if x is palindrome integer.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-An integer is a palindrome when it reads the same backward as forward.
-
-For example, 121 is a palindrome while 123 is not.
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
 
 */
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+class Solution {
+public:
+    map<char,int> dict = {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000},
+    };
+
+    int romanToInt(string s) {
+        vector<int> vec;
+        for(int i=0;i<s.size();i++){
+            char x = s.at(i);
+            if(dict.count(x)!=0){
+                int temp = this->dict[x];
+                vec.push_back(temp);
+            }
+            else{
+                cout << "error:" << x <<endl;
+            }
+
+        }
+        for(auto x: vec){
+            cout << x <<endl;
+        }
+        return 1;
+    }
+
+};
+int main(int argc, char** argv){
+    vector<string> opt;
+    for(int i=1;i<argc;i++){
+        opt.push_back(string(argv[i]));
+    }
+    Solution solution;
+    for(auto x:opt){
+        cout <<solution.romanToInt(x) <<endl;
+    }
+    return EXIT_SUCCESS;
+}
