a2d172ed24486c836112067565cabee2d34c3eb7
Đỗ Viết Chính <dovietchinh1998@gmail.com>
2023-11-29 16:30:29 +0700

    Create main.py
    1299 problems

---
+++ problems/1299. Replace Elements with Greatest Element on Right Side/main.py
@@ -0,0 +1,25 @@
+from typing import List
+import random
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        
+        if len(arr) == 1:
+            return [-1]
+        new_arr = []
+        new_arr.append(-1)
+        max_value = arr[-1]
+        for index in range(len(arr)-1,0,-1):
+            if max_value < arr[index-1]:
+                max_value = arr[index-1]
+            new_arr.insert(0,max_value)
+        return new_arr
+
+if __name__ == '__main__':
+    arr = random.sample(range(100),10)
+    a = Solution().replaceElements(arr)
+    print(arr)
+    print(a)
+
+        
+
+
