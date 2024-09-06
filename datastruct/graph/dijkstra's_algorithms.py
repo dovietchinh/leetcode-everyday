1b2cedb6adb459d114b792cc1bde523473487b2d
sheldon <>
2024-09-06 12:48:31 +0800

    fix bug in dijkstra algorithms

--- datastruct/graph/dijkstra's_algorithms.py
+++ datastruct/graph/dijkstra's_algorithms.py
@@ -3,26 +3,40 @@ import random
 
 # find the sortest path from a source node to all other nodes in a graph 
 
-def dijkstra_algorithms(graph,start,end):
-    size = len(graph)
-    table = [100 for _ in range(size)]
-    table[start] = 0
-    visited = [False for _ in range(size)]
+def dijkstra_algorithms(graph,start):
+    visited = [False]*len(graph)
+    table = [[None,None]]*len(graph)
+    table[start] = [0,None]
     queue = [start]
     while queue:
         node = queue.pop(0)
         if visited[node]:
             continue
         visited[node] = True
+        distance,previous = table[node]
+        if distance is None:
+            continue
         for neighbor,weight in graph[node]:
-            print('neighbor,weight of node: ',neighbor,weight,node)
+            distance_neighbor,previous_neighbor = table[neighbor]
+            print('distance_neighbor: ',distance_neighbor)
+            print('previous_neighbor: ',previous_neighbor)
+            print('weight: ',weight)
+            print('distance: ',distance)
+            print('previous: ',previous)
+            if distance_neighbor is None:
+                distance_neighbor = weight + distance
+                previous_neighbor = node
+                table[neighbor] = [distance_neighbor,previous_neighbor]
+            elif distance_neighbor > weight + distance:
+                distance_neighbor = weight + distance
+                previous_neighbor = node
+                table[neighbor] = [distance_neighbor,previous_neighbor]
+            else:
+                pass 
             queue.append(neighbor)
-            print('queue',queue)
-            print('table[node]',table[node])
-            print('table[neighbor]',table[neighbor])
-            print('weight',weight)
-            table[neighbor] = min(table[neighbor],table[node]+weight)
-    print(table)            
+        
+    return table 
+            
     
 import matplotlib.pyplot as plt
 
@@ -38,30 +52,52 @@ def visualize_graph(graph):
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     
     plt.show()                
-# if __name__ == '__main__':
-#     graph = [[(1,1),(2,4)],[(0,1),(2,2),(3,5)],[(0,4),(1,2),(3,1)],[(1,5),(2,1)]]
-#     start = 0
-    # end = 3
-    
-
 if __name__ == '__main__':
     # graph = [[(1,1),(2,4)],[(0,1),(2,2),(3,5)],[(0,4),(1,2),(3,1)],[(1,5),(2,1)]]
     
     def generate_complex_graph(graph, num_nodes, num_edges):
-        for _ in range(num_edges):
-            node1 = random.randint(0, num_nodes-1)
-            node2 = random.randint(0, num_nodes-1)
+        # Generate a connected graph with random weights
+        for i in range(num_nodes - 1):
+            # Connect each node to the next node
             weight = random.randint(1, 10)
-            graph[node1].append((node2, weight))
-            graph[node2].append((node1, weight))
+            graph[i].append((i + 1, weight))
+            graph[i + 1].append((i, weight))
+        
+        # Add remaining edges randomly
+        while len(get_all_edges(graph)) < num_edges:
+            node1 = random.randint(0, num_nodes - 1)
+            node2 = random.randint(0, num_nodes - 1)
+            if node1 != node2 and not is_edge_exists(graph, node1, node2):
+                weight = random.randint(1, 10)
+                graph[node1].append((node2, weight))
+                graph[node2].append((node1, weight))
+        
+        return graph
 
-    num_nodes = 8
+    def get_all_edges(graph):
+        edges = set()
+        for node, neighbors in enumerate(graph):
+            for neighbor, _ in neighbors:
+                edge = tuple(sorted((node, neighbor)))
+                edges.add(edge)
+        return edges
+
+    def is_edge_exists(graph, node1, node2):
+        for neighbor, _ in graph[node1]:
+            if neighbor == node2:
+                return True
+        return False
+
+
+    num_nodes = 7
     num_edges = 14
     graph = [[] for _ in range(num_nodes)]
     generate_complex_graph(graph, num_nodes, num_edges)
+    print('graph: ',graph)
     start = 0
     end = 3
-    dijkstra_algorithms(graph, start, end)
+    table = dijkstra_algorithms(graph, start)
+    print('table :',table)
     visualize_graph(graph)
     
 
