8ac59490da49419ec75d605f5dd5363d95ae06a1
dovietchinh <dovietchinh1998@gmail.com>
2024-09-12 19:40:30 +0800

    problems 215 done

--- problems/Medium/215. Kth Largest Element in an Array/main.py
+++ problems/Medium/215. Kth Largest Element in an Array/main.py
@@ -1,9 +1,16 @@
-from typing import *
+import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pass
+        queue = []
+        size = len(nums)
+        for i in nums:
+            heapq.heappush(queue,i*-1)
+            # print('queue: ',queue)
+        for i in range(k):
+            r = heapq.heappop(queue)
+        return -r
 
-
+        
 
 if __name__ == '__main__':
     pass
