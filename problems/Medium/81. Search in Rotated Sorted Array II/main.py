32606de7ac4a071062ca93bd7a4745539f9b6cfa
dovietchinh <dovietchinh1998@gmail.com>
2024-09-12 12:20:33 +0800

    problems 81 done

--- problems/Medium/81. Search in Rotated Sorted Array II/main.py
+++ problems/Medium/81. Search in Rotated Sorted Array II/main.py
@@ -5,39 +5,35 @@ class Solution:
         left,right = 0, size - 1
         while left <= right: 
             mid = (left + right) // 2 
-            # print('left:', left, 'right:', right, 'mid:', mid)
+            print('left:', left, 'right:', right, 'mid:', mid)
+            print('nums[left]:', nums[left], 'nums[right]:', nums[right], 'nums[mid]:', nums[mid])
             if target in [nums[mid], nums[left], nums[right]]:
                 return True 
-            if nums[left] > nums[mid]: 
-                # pivot point in range(left-mid)
-                if nums[left] < target:
-                    right = mid - 1
-                elif target < nums[mid]:
-                    right = mid - 1
-                elif target < nums[right]:
+            if nums[left] == nums[right]:
+                left += 1
+                right -= 1
+            elif nums[left] < nums[right]:
+                ## normal binary search
+                if target < nums[mid]:
                     right = mid - 1
                 else:
-                    left += 1
-                    right -= 1
-            elif nums[mid] == nums[left]:
-                if nums[mid] < nums[right]:
                     left = mid + 1
-                elif nums[mid] > nums[right]:
-                    right = mid - 1
-                else: 
-                    left += 1
-                    right -= 1
-            else: 
-                # pivot point in range(mid-right)
-                if nums[left] < target:
-                    right = mid - 1
-                elif target > nums[mid]:
-                    left = mid + 1
-                elif target < nums[right]:
-                    left = mid + 1
+
+            elif nums[left] > nums[right]:
+                #pivot in range
+                if nums[left] > nums[mid]:
+                    #pivot in left
+                    if target < nums[mid] or target > nums[left]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
                 else:
-                    left += 1
-                    right -= 1
+                    print('pivot in right')
+                    if target < nums[right] or target > nums[mid]:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+                    
         return False
 
 
@@ -53,17 +49,26 @@ def generate_test():
 
 
 if __name__ == '__main__':
-    from tqdm import tqdm
-    for index in tqdm(range(100),total=100):
-        arr,target,label = generate_test() 
-        print(index)
-        r = Solution().search(arr, target)
+    # from tqdm import tqdm
+    # for index in tqdm(range(100),total=100):
+    #     arr,target,label = generate_test() 
+    #     print(index)
+    #     r = Solution().search(arr, target)
         
-        if r!=label:
-            print(arr, target, label, r)
-            break
-    # r = Solution().search(nums, target)
-    # print(r)
+    #     if r!=label:
+    #         print(arr, target, label, r)
+    #         break
 
-    [559, 583, 604, 607, 610, 611, 612, 615, 622, 645, 651, 664, 672, 673, 692, 699, 701, 704, 727, 729, 729, 765, 766, 774, 787, 792, 795, 798, 803, 827, 838, 840, 845, 855, 856, 863, 907, 908, 913, 915, 920, 922, 930, 933, 940, 941, 945, 983, 997, 2, 5, 14, 28, 39, 39, 48, 63, 67, 73, 76, 85, 128, 128, 137, 145, 158, 161, 170, 179, 184, 187, 188, 198, 204, 256, 260, 295, 336, 339, 352, 357, 382, 392, 402, 413, 415, 418, 423, 434, 441, 450, 451, 455, 458, 464, 478, 482, 500, 509, 539] 
-    699
+    nums = [1,1,1,1,1,1,0,1]
+    target = 0.5
+    r = Solution().search(nums, target)
+    print(r)
+    pass
+
+    # arr = [659, 662, 682, 689, 701, 721, 725, 730, 732, 739, 744, 747, 748, 752, 757, 766, 774, 779, 781, 787, 802, 830, 830, 839, 864, 874, 874, 874, 880, 888, 915, 926, 927, 939, 952, 965, 970, 975, 981, 983, 996, 9, 51, 59, 70, 83, 88, 96, 103, 106, 107, 112, 124, 142, 147, 153, 165, 166, 177, 186, 202, 211, 217, 231, 231, 253, 259, 267, 284, 284, 301, 330, 333, 347, 353, 366, 367, 372, 392, 406, 429, 433, 433, 451, 502, 503, 506, 535, 542, 552, 566, 567, 586, 591, 592, 606, 621, 634, 643, 649] 
+    # target = 965
+    # r = Solution().search(arr, target)
+    # print(r)
+    # print(arr.index(target))
+    # print(arr.index(max(arr)))
+    
