dd7dc646ff4b8d1bd683589d31a1ac6c7e748cfe
sheldon <>
2024-10-10 12:50:43 +0800

    problems 135 in progress

---
+++ problems/Hard/135. Candy/main.py
@@ -0,0 +1,10 @@
+from typing import *
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        pass
+
+if __name__ == '__main__':
+    ratings = [1,3,4,5,2]
+    r = Solution().candy(ratings)
+    print(r)
+        
